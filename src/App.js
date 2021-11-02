4b1b7dc3dc192a9fd9dc0c6a3097a05ddd852986
juned03 <alikhanjuned807@gmail.com>
2021-11-02 16:01:33 +0530

    [ADDED] the lading page

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import png_small from "./png-small.png";
+import photo from "./photo.png";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      class="
+        d-flex
+        flex-wrap
+        align-items-center
+        justify-content-evenly
+      "
+      style={{
+        backgroundColor: "#e3e5e0",
+        height: "100vh",
+        width: "100%",
+        flex: 1,
+      }}
+    >
+      <div class="logontext-container d-flex flex-column align-items-center">
+        <img src={png_small} alt="" />
+        <div class="text-center">
+          <h1 class="heading">The Adventure Begins Soon..</h1>
+          <p class="sub-heading">watch out the space begins for more</p>
+        </div>
+      </div>
+      <div class="cloveimg-container">
+        <img className="clove" src={photo} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
